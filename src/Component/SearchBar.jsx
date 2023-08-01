cc3d2f94bd60f430c232a71e7726c6f067103f8b
pankaj upadhyay <pankajup.git01@gmail.com>
2023-08-02 00:31:07 +0530

    icons updated

--- src/Component/SearchBar.jsx
+++ src/Component/SearchBar.jsx
@@ -1,11 +1,10 @@
-import React from "react";
-import { FaSearch } from 'react-icons/fa';
+import React from "react"; 
 const SearchBar = () => {
     return (
         <div>
             <form className='flex justify-center m-auto' >
                 <input type="text" placeholder='Search your product..' className='text-center sm:p-[0.5rem_1.5rem] md:w-[310px] focus:outline-none border-[1px] border-gray-400 rounded-bl-lg rounded-tl-lg' />
-                <button className='bg-blue-800 py-[10px] md:py-[5px] px-[20px] rounded-br-lg rounded-tr-lg text-white text-xl'><FaSearch /></button>
+                <button className='bg-blue-800 py-[10px] md:py-[5px] px-[20px] rounded-br-lg rounded-tr-lg text-white flex items-center font-bold text-2xl'><ion-icon name="search"></ion-icon></button>
             </form>
             
         </div>
