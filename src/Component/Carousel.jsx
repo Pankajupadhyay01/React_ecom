4e7791cb683b21929530cf6e08bc2cc0e1015742
pankaj upadhyay <pankajup.git01@gmail.com>
2023-08-09 11:16:31 +0530

    home crousel completed

--- src/Component/Carousel.jsx
+++ src/Component/Carousel.jsx
@@ -1,21 +1,33 @@
-import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import React, { useState } from 'react' 
 const Carousel = () => {
-    const products = useSelector((state) => state.user.product);
+    const images = [
+        {
+            img:'/assets/c1.jpg'
+        },
+        {
+            img:'/assets/c2.jpg'
+        },
+        {
+            img:'/assets/c3.jpg'
+        },
+        {
+            img:'/assets/c4.jpg'
+        },
+    ] 
     const [slide, setslide] = useState(0)
 
     setTimeout(() => {
-        setslide(slide<products.length-1?slide+1:0)
+        setslide(slide<images.length-1?slide+1:0)
     }, 4500);
     return (
         <div className='h-[65vh] '>
             <div className='w-full h-full relative overflow-hidden flex'>
                 <div className={`h-full flex absolute left-[0]`} style={{ transform: `translateX(-${slide * 100}vw)` }}>
-                    {products.map((pro, i) => (
+                    {images.map((pro, i) => (
 
                         <div key={i} className='transition ease-in-out delay-150 w-[100vw] flex items-center justify-center '>
-                            <div className='w-full h-full bg-[crimson]'>
-                                <img src={pro?.images[0]} alt="" srcSet="" /> 
+                            <div className='w-full object-cover h-full bg-blue-50'>
+                                <img className='h-full w-full' src={pro.img} alt="Crousel Banner" srcSet="" /> 
                             </div>
                         </div>
                     ))
